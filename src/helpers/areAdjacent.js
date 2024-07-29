3bbdef048882c654fbdbf77829ae3ba1b21222ab
GrabovyiS <graboviys@yandex.ru>
2024-07-29 22:47:33 +0300

    fix: correctly determine coordinates adjacency

--- src/helpers/areAdjacent.js
+++ src/helpers/areAdjacent.js
@@ -2,13 +2,22 @@ const areAdjacent = (coords1, coords2) => {
   if (
     Math.abs(coords1[0] - coords2[0]) <= 1 &&
     Math.abs(coords1[1] - coords2[1]) <= 1 &&
-    coords1[0] !== coords2[0] &&
-    coords1[1] !== coords2[1]
+    !coordsAreTheSame(coords1, coords2)
   ) {
     return true;
   }
 
   return false;
+};
+
+const coordsAreTheSame = (coords1, coords2) => {
+  for (let i = 0; i < coords1.length; i++) {
+    if (coords1[i] !== coords2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 export default areAdjacent;
--- src/helpers/areAdjacent.test.js
+++ src/helpers/areAdjacent.test.js
@@ -2,8 +2,10 @@ import areAdjacent from './areAdjacent';
 
 test('Return true when adjacent', () => {
   expect(areAdjacent([2, 3], [3, 4])).toBe(true);
+  expect(areAdjacent([3, 5], [3, 6])).toBe(true);
 });
 
 test('Return false when are not adjacent', () => {
   expect(areAdjacent([2, 3], [4, 5])).toBe(false);
+  expect(areAdjacent([2, 3], [2, 5])).toBe(false);
 });
