7e18f13e303f9ab3058f2dd3a430beb19fa26e6c
GrabovyiS <graboviys@yandex.ru>
2024-07-29 22:54:50 +0300

    feat: computer player hitting for adjacent squares after hit

---
+++ src/data/ComputerPlayer.js
@@ -0,0 +1,78 @@
+import Player from './Player';
+import getRandomCoords from '../helpers/getRandomCoords';
+import forEachAdjacent from '../helpers/forEachAdjacent';
+
+const ComputerPlayer = (opponent) => {
+  const computerPlayer = Player();
+
+  computerPlayer.opponent = opponent;
+
+  computerPlayer.lastHitShipCoords;
+
+  computerPlayer.makeTurn = function () {
+    let hitCoords;
+
+    if (this.lastHitShipCoords) {
+      const lastHit = this.lastHitShipCoords;
+      const topBottomAdjacentCoords = [];
+      topBottomAdjacentCoords.push([lastHit[0], lastHit[1] + 1]);
+      topBottomAdjacentCoords.push([lastHit[0] + 1, lastHit[1]]);
+      topBottomAdjacentCoords.push([lastHit[0], lastHit[1] - 1]);
+      topBottomAdjacentCoords.push([lastHit[0] - 1, lastHit[1]]);
+
+      while (true) {
+        const index = Math.floor(Math.random() * 4);
+        const boardSize = this.opponent.gameboard.board.length;
+
+        if (
+          topBottomAdjacentCoords[index][0] > boardSize - 1 ||
+          topBottomAdjacentCoords[index][0] < 0 ||
+          topBottomAdjacentCoords[index][1] > boardSize - 1 ||
+          topBottomAdjacentCoords[index][1] < 0
+        ) {
+          continue;
+        }
+
+        hitCoords = [
+          topBottomAdjacentCoords[index][0],
+          topBottomAdjacentCoords[index][1],
+        ];
+
+        if (
+          this.opponent.gameboard.hitsBoard[hitCoords[0]][hitCoords[1]] === null
+        ) {
+          break;
+        }
+      }
+
+      return this.opponent.gameboard.receiveAttack(hitCoords);
+    }
+
+    while (true) {
+      hitCoords = getRandomCoords();
+
+      // if found a ship on a square that hasn't been hit before
+      if (
+        this.opponent.gameboard.board[hitCoords[0]][hitCoords[1]] !== null &&
+        typeof this.opponent.gameboard.board[hitCoords[0]][hitCoords[1]] ===
+          'object' &&
+        this.opponent.gameboard.hitsBoard[hitCoords[0]][hitCoords[1]] === null
+      ) {
+        this.lastHitShipCoords = [hitCoords[0], hitCoords[1]];
+      }
+
+      // if found a square what hasn't been hit before
+      if (
+        this.opponent.gameboard.hitsBoard[hitCoords[0]][hitCoords[1]] === null
+      ) {
+        break;
+      }
+    }
+
+    return this.opponent.gameboard.receiveAttack(hitCoords);
+  };
+
+  return computerPlayer;
+};
+
+export default ComputerPlayer;
---
+++ src/data/ComputerPlayer.test.js
@@ -0,0 +1,93 @@
+import ComputerPlayer from './ComputerPlayer';
+import Player from './Player';
+import Ship from './Ship';
+import areAdjacent from '../helpers/areAdjacent';
+
+test('Shoots', () => {
+  const opponent = Player();
+  const computerPlayer = ComputerPlayer(opponent);
+  computerPlayer.makeTurn();
+
+  let oneSquareIsHit = false;
+
+  for (const row of opponent.gameboard.hitsBoard) {
+    for (const square of row) {
+      if (square !== null) {
+        oneSquareIsHit = true;
+      }
+    }
+  }
+
+  expect(oneSquareIsHit).toBe(true);
+});
+
+test('Does not shoot at the same square twice', () => {
+  const opponent = Player();
+  const computerPlayer = ComputerPlayer(opponent);
+
+  // Shoot every square
+  for (
+    let i = 0;
+    i <
+    computerPlayer.gameboard.board.length *
+      computerPlayer.gameboard.board.length;
+    i++
+  ) {
+    expect(computerPlayer.makeTurn()).not.toBeInstanceOf(Error);
+  }
+});
+
+test('Tries one of adjacent squares after hit', () => {
+  const opponent = Player();
+  const computerPlayer = ComputerPlayer(opponent);
+
+  const longShip = Ship(4);
+
+  opponent.gameboard.placeShip(longShip, [3, 3], 'ttb');
+  opponent.gameboard.placeShip(longShip, [3, 5], 'ttb');
+  opponent.gameboard.placeShip(longShip, [3, 7], 'ttb');
+
+  let hit = false;
+  let hitSquareCoords = null;
+  while (hit === false) {
+    computerPlayer.makeTurn();
+
+    for (let i = 0; i < opponent.gameboard.hitsBoard.length; i++) {
+      for (let j = 0; j < opponent.gameboard.hitsBoard.length; j++) {
+        const square = opponent.gameboard.hitsBoard[i][j];
+        if (square === 'hit') {
+          hit = true;
+          hitSquareCoords = [i, j];
+        }
+      }
+    }
+  }
+
+  const boardAfterFirstHit = [];
+  for (let i = 0; i < opponent.gameboard.board.length; i++) {
+    boardAfterFirstHit.push(opponent.gameboard.hitsBoard[i].slice());
+  }
+
+  console.table(boardAfterFirstHit);
+
+  computerPlayer.makeTurn();
+  let newlyHitSquareCoords = null;
+
+  for (let i = 0; i < opponent.gameboard.hitsBoard.length; i++) {
+    for (let j = 0; j < opponent.gameboard.hitsBoard.length; j++) {
+      const newSquare = opponent.gameboard.hitsBoard[i][j];
+      const oldSquare = boardAfterFirstHit[i][j];
+
+      if (newSquare !== oldSquare) {
+        newlyHitSquareCoords = [i, j];
+      }
+    }
+  }
+
+  console.table(opponent.gameboard.hitsBoard);
+
+  console.log('new hit:', newlyHitSquareCoords);
+  console.log('old hit:', hitSquareCoords);
+
+  expect(areAdjacent(hitSquareCoords, newlyHitSquareCoords)).toBe(true);
+});
